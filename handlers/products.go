6f10520af7095f6f8d76a89123a2efb94687ac23
dias1397 <joao.dias1397@gmail.com>
2021-04-27 22:38:44 +0100

    POST and PUT implementation

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/dias1397/product-api/data"
 )
@@ -17,18 +19,80 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodGet {
-		p.GetProducts(rw, req)
+		p.getProducts(rw, req)
 		return
 	}
 
+	if req.Method == http.MethodPost {
+		p.addProduct(rw, req)
+		return
+	}
+
+	if req.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(req.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, req)
+	}
+
+	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) GetProducts(rw http.ResponseWriter, req *http.Request) {
+func (p *Products) getProducts(rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle GET Products")
 	lp := data.GetProducts()
 
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle POST Product")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle POST Product")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product Not Found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product Not Found", http.StatusInternalServerError)
+		return
 	}
 }
